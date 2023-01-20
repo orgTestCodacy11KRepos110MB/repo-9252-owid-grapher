e281d5c65f6b5af7a1a7a8d88d5d0c7b628c4752
Daniel Bachler <daniel@danielbachler.de>
2023-01-20 13:47:57 +0100

    feat: complete initial version of creating gdoc documents from old posts

--- site/gdocs/ArticleBlock.tsx
+++ site/gdocs/ArticleBlock.tsx
@@ -248,6 +248,9 @@ export default function ArticleBlock({
                         />
                     )
                 })
+                .with({ type: "simple-text" }, (block) => {
+                    return block.value
+                })
                 .with({ type: "heading", level: 1 }, (block) => (
                     <h1
                         className={cx(
